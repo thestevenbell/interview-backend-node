403d8c28785c63330ff013ac5cbbd86fff70321b
Steven Bell <thestevenbell@gmail.com>
2016-04-07 16:29:58 -0400

    completed pulling JSON from API

--- README.md
+++ README.md
@@ -8,3 +8,16 @@ Once the pull request has been evaluated, it will be closed and someone will be 
 
 Thanks!
 
+## Directions for use of this Node Module:
+
+_Step 1_  Clone the repo locally
+_Step 2_  run $`npm install` from within the working directory
+_Step 4_
+_Step 5_
+_Step 6_
+
+
+> Notes:
+> added the via NPM: morgan, express and express.
+>
+>
--- main.js
+++ main.js
@@ -1,65 +1,51 @@
-'use strict';
+// 'use strict';
+var morgan = require('morgan');
+var express = require('express');
+var request = require('request');
 
-/*
- * ## Task 1 (of 2)
- *
- * Create a script starting in this file, but not limited to, that will pull
- * data from a remote source JSON feed, transform it, then output a different
- * JSON feed matching a specific schema.  This should execute when running
- * `npm start` and should log output to the console.
- *
- *
- * ### Source:
- *
- *     http://www.cnn.com/data/ocs/section/index.html:homepage1-zone-1.json
- *
- *
- * ### Output:
- *
- *     A valid JSON string representing an array of articles from the
- *     "Top stories" container with the following properties for each article:
- *
- *         {
- *             "url": String,
- *             "headline": String,
- *             "imageUrl": String,
- *             "byLine": String
- *         }
- *
- *
- * ### Other information and guidelines:
- *
- * - Use the node version specified by the engines property in package.json.
- *
- * - `url` should include the protocol and domain, which would be www.cnn.com.
- *
- * - Not all articles will have an image.  If an article has something like a
- *   gallery or a video, use your best judgement.
- *
- * - The `imageUrl` should reference the highest quality image available.
- *
- * - The `byLine` should contain information about the author of the article,
- *   which may not be available for every article.
- *
- * - The source feed will represent what is on the top of the www.cnn.com
- *   homepage.  The content to map is in a _container_ with a _title_ of
- *   "Top stories".  This content changes all the time.  The placement of
- *   this container will change as well.
- *
- *
- *
- *
- * ## Task 2 (of 2)
- *
- * Configure the project to execute ESLint on all JavaScript files in the
- * project using the included .eslintrc configuration when `npm test` is run.
- * DO NOT use Grunt or Gulp.  The test should pass.
- *
- *
- *
- *
- * ## Extra Credit
- *
- * Write a unit test to validate that the JSON ouput is valid and matches the
- * expected schema.
- */
+var app = express();
+
+// app.locals.newsfeed = require('./newsfeed.json');
+
+app.use(morgan('combined'));
+
+
+var url = 'http://www.cnn.com/data/ocs/section/index.html:homepage1-zone-1.json';
+
+console.log('Request "GET" to url:' + url);
+
+var cnnJSON = [];
+// var str = [];
+
+  // used the request module to ping CNN.com for JSON data
+  // if no error and status is 200 store the JSON object to a variable
+  // parsed JSON object to allow manipulation of the object
+  // stores the entire JSON object to the global variable
+request(url, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var parsedJSONres = JSON.parse(body);
+    cnnJSON.push(parsedJSONres);
+    // logoutcnnJSON();
+    extractValues();
+  }
+  else {
+    console.log(response.statusCode);
+  }
+});
+
+// function logoutcnnJSON() {
+//     console.log('cnnJSON: ' + cnnJSON);
+//     str = JSON.stringify(cnnJSON, null, 4);
+//     // console.log(str);
+// }
+
+
+function extractValues () {
+  console.log(cnnJSON[0].zoneContents[0].id);
+
+}
+
+
+
+
+
